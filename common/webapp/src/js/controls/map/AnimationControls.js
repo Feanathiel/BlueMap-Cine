0f2a71db9b870192fccd0f348dffb1c8b457c39a
Feanathiel <Feanathiel@users.noreply.github.com>
2025-12-15 20:22:48 +0100

    From params

--- common/webapp/src/js/controls/map/AnimationControls.js
+++ common/webapp/src/js/controls/map/AnimationControls.js
@@ -70,6 +70,17 @@ export class AnimationControls {
                     duration: parseInt(parsedParams.get("duration"))
                 }
             }
+        } else if (mode === "cine") {
+            // localhost:5173/#world:0:0:0:0:0:0:0:0:animation/mode=cine&autoStart=true&showUi=false&data=eyJzY2VuZXMiOlt7ImR1cmF0aW9uIjoxODAwMCwidHJhY2tzIjpbeyJwcm9wZXJ0eSI6Im92ZXJsYXkub3BhY2l0eSIsImtleWZyYW1lcyI6W3sidGltZSI6MCwiaW50ZXJwb2xhdGlvbiI6ImxpbmVhciIsInZhbHVlIjoxfSx7InRpbWUiOjMwMDAsImludGVycG9sYXRpb24iOiJsaW5lYXIiLCJ2YWx1ZSI6MH0seyJ0aW1lIjoxNTAwMCwiaW50ZXJwb2xhdGlvbiI6ImxpbmVhciIsInZhbHVlIjowfSx7InRpbWUiOjE4MDAwLCJpbnRlcnBvbGF0aW9uIjoibGluZWFyIiwidmFsdWUiOjF9XX0seyJwcm9wZXJ0eSI6ImNhbWVyYS5wb3NpdGlvbiIsImtleWZyYW1lcyI6W3sidGltZSI6MCwiaW50ZXJwb2xhdGlvbiI6ImxpbmVhciIsInZhbHVlIjp7IngiOjU1Ljg0MDUwMDUwNTY3NTM2LCJ5Ijo1OS4xNzg1MzY0NjMxOTI3OCwieiI6LTE1NTAuMjUzMzY0OTM5NTA0NX19LHsidGltZSI6MTgwMDAsImludGVycG9sYXRpb24iOiJsaW5lYXIiLCJ2YWx1ZSI6eyJ4IjozMC44NjE3MzQ3MzUyODE2MSwieSI6MTEzLjM1NSwieiI6LTE1NDUuMjk3MTMxNTM0NDQzNX19XX0seyJwcm9wZXJ0eSI6ImNhbWVyYS5hbmdsZSIsImtleWZyYW1lcyI6W3sidGltZSI6MCwiaW50ZXJwb2xhdGlvbiI6ImxpbmVhciIsInZhbHVlIjowLjg4NDAxNjQxNjY1NzE1NTJ9LHsidGltZSI6MTgwMDAsImludGVycG9sYXRpb24iOiJsaW5lYXIiLCJ2YWx1ZSI6MS40MDM3MTU0MjU2MzA4Njk1fV19LHsicHJvcGVydHkiOiJjYW1lcmEucm90YXRpb24iLCJrZXlmcmFtZXMiOlt7InRpbWUiOjAsImludGVycG9sYXRpb24iOiJsaW5lYXIiLCJ2YWx1ZSI6MS41ODMzNjI4MzUyNDg5Nzk1fSx7InRpbWUiOjE4MDAwLCJpbnRlcnBvbGF0aW9uIjoibGluZWFyIiwidmFsdWUiOjEuNTc4OTQ4NjQ5Njg3MTI0M31dfSx7InByb3BlcnR5IjoiY2FtZXJhLmRpc3RhbmNlIiwia2V5ZnJhbWVzIjpbeyJ0aW1lIjowLCJpbnRlcnBvbGF0aW9uIjoibGluZWFyIiwidmFsdWUiOjIzOC4xNDgwNjg3NDY5MzQ2NX0seyJ0aW1lIjoxODAwMCwiaW50ZXJwb2xhdGlvbiI6ImxpbmVhciIsInZhbHVlIjo1fV19XX0seyJkdXJhdGlvbiI6MTAwMDAsInRyYWNrcyI6W3sicHJvcGVydHkiOiJvdmVybGF5Lm9wYWNpdHkiLCJrZXlmcmFtZXMiOlt7InRpbWUiOjAsImludGVycG9sYXRpb24iOiJsaW5lYXIiLCJ2YWx1ZSI6MX0seyJ0aW1lIjoyMDAwLCJpbnRlcnBvbGF0aW9uIjoibGluZWFyIiwidmFsdWUiOjB9LHsidGltZSI6ODAwMCwiaW50ZXJwb2xhdGlvbiI6ImxpbmVhciIsInZhbHVlIjowfSx7InRpbWUiOjEwMDAwLCJpbnRlcnBvbGF0aW9uIjoibGluZWFyIiwidmFsdWUiOjF9XX0seyJwcm9wZXJ0eSI6ImNhbWVyYS5wb3NpdGlvbiIsImtleWZyYW1lcyI6W3sidGltZSI6MCwiaW50ZXJwb2xhdGlvbiI6ImxpbmVhciIsInZhbHVlIjp7IngiOjM4LjEzMjQxNTc2OTE4NTgyNiwieSI6MTEyLjMzNDI1NjI5ODI5MTgsInoiOi0xNTc1LjIyODQwMzIxMDAyN319LHsidGltZSI6MTAwMDAsImludGVycG9sYXRpb24iOiJsaW5lYXIiLCJ2YWx1ZSI6eyJ4Ijo0MC44ODAxMTQ5NjM1NTczNzUsInkiOjExMC4xODAxMzgxNjMyMjU5LCJ6IjotMTUyNS44MjYxNjgzMDM5NTExfX1dfSx7InByb3BlcnR5IjoiY2FtZXJhLmFuZ2xlIiwia2V5ZnJhbWVzIjpbeyJ0aW1lIjowLCJpbnRlcnBvbGF0aW9uIjoibGluZWFyIiwidmFsdWUiOjEuNDU1MzYzNTMyMDU5NTE0NH1dfSx7InByb3BlcnR5IjoiY2FtZXJhLnJvdGF0aW9uIiwia2V5ZnJhbWVzIjpbeyJ0aW1lIjowLCJpbnRlcnBvbGF0aW9uIjoibGluZWFyIiwidmFsdWUiOjEuNTY3MzI4MTA3NjU1NDM1M31dfSx7InByb3BlcnR5IjoiY2FtZXJhLmRpc3RhbmNlIiwia2V5ZnJhbWVzIjpbeyJ0aW1lIjowLCJpbnRlcnBvbGF0aW9uIjoibGluZWFyIiwidmFsdWUiOjE1Ljh9XX1dfV19
+
+            this.init = {
+                params: {
+                    mode: mode,
+                    autoStart: parsedParams.get("autoStart") === "true",
+                    showUi: parsedParams.get("showUi") === "true",
+                    data: parsedParams.get("data")
+                }
+            };
         }
     }
 
@@ -108,7 +119,12 @@ export class AnimationControls {
 
     postInit() {
         if (this.init && this.manager) {
-            this.init.animations = this._initFromObjectRotateMode(this.init.params);
+            if (this.init.params.mode === "rotate") {
+                this.init.animations = this._initFromObjectRotateMode(this.init.params);
+            } else if (this.init.params.mode === "cine") {
+                this.init.animations = this._initFromCineJson(this.init.params);
+            }
+
             this._loadFromInit(this.init.animations);
 
             if (this.init.params.autoStart) {
@@ -197,6 +213,10 @@ export class AnimationControls {
                         if (maxKeyFrameValue === null || minKeyFrameValue.time > keyFrameValue.time) {
                             maxKeyFrameValue = keyFrameValue;
                         }
+
+                        // Cubic is still broken, 'cause it tries to tween between scenes, which it shouldn't do.
+                        // Probably we can add a dummy keyframe.
+                        keyFrameValue.interpolation = "linear";
 
                         keyFrameValues.push(keyFrameValue);
                     }
@@ -346,6 +366,11 @@ export class AnimationControls {
                 },
             ],
         };
+    }
+
+    _initFromCineJson(params) {
+        const {data} = params;
+        return JSON.parse(atob(data));
     }
 
     _mapToTargetProperty(prop) {
